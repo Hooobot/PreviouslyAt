195a6dce49dfee4e06ea6082d9afe19ba3e8226e
Hooobot <hoobot0@gmail.com>
2023-09-27 12:58:37 -1000

    Modify App.js to utilize canvas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,48 @@
-import React, { useRef, useEffect } from 'react';
-import './App.css';
-import IconList from './IconList';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import IconList from "./IconList";
+import { useCanvas } from "./Canvas";
 
 const App = () => {
   const canvasRef = useRef(null);
+  const { icons, addIcon } = useCanvas();
 
   const handleDrop = (e) => {
+    e.preventDefault();
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const iconName = e.dataTransfer.getData('iconName');
-
+    const iconName = e.dataTransfer.getData("iconName");
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-
-    // Draw icon based on iconName (For now, let's draw text)
-    ctx.fillText(iconName, x, y);
+    addIcon(iconName, x, y);
   };
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    let rectX = 10, rectY = 10;  // Current position of the rectangle
-    let offsetX = 0, offsetY = 0;  // Offset from the top-left corner of the rectangle
-    let isDragging = false;  // Whether the rectangle is being dragged
+    const ctx = canvas.getContext("2d");
+
+    let rectX = 10,
+      rectY = 10;
+    let offsetX = 0,
+      offsetY = 0;
+    let isDragging = false;
+
+    const drawIcons = () => {
+      icons.forEach((icon) => {
+        ctx.fillText(icon.iconName, icon.x, icon.y);
+      });
+    };
 
     const drawRect = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);  // Clear the canvas
-      ctx.fillStyle = '#f00';
-      ctx.fillRect(rectX, rectY, 50, 50);  // Draw the rectangle at its current position
+      ctx.fillStyle = "#f00";
+      ctx.fillRect(rectX, rectY, 50, 50);
     };
 
     const handleMouseDown = (e) => {
       const rect = canvas.getBoundingClientRect();
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top;
-      
+
       if (x >= rectX && x <= rectX + 50 && y >= rectY && y <= rectY + 50) {
         isDragging = true;
         offsetX = x - rectX;
@@ -55,35 +62,39 @@ const App = () => {
 
         rectX = x - offsetX;
         rectY = y - offsetY;
-
-        drawRect();
+        renderCanvas();
       }
     };
 
-    drawRect();  // Initial drawing
+    const renderCanvas = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawRect();
+      drawIcons();
+    };
 
-    canvas.addEventListener('mousedown', handleMouseDown);
-    canvas.addEventListener('mouseup', handleMouseUp);
-    canvas.addEventListener('mousemove', handleMouseMove);
+    renderCanvas();
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      canvas.removeEventListener('mousedown', handleMouseDown);
-      canvas.removeEventListener('mouseup', handleMouseUp);
-      canvas.removeEventListener('mousemove', handleMouseMove);
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+      canvas.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);
+  }, [icons]);
 
- return (
+  return (
     <div className="App">
       <header className="App-header">
         <div className="icon-list-wrapper">
           <IconList />
         </div>
-        <canvas 
-          ref={canvasRef} 
-          width={600} 
-          height={400} 
-          style={{ border: '2px solid #000' }}
+        <canvas
+          ref={canvasRef}
+          width={600}
+          height={400}
+          style={{ border: "2px solid #000" }}
           onDrop={handleDrop}
           onDragOver={(e) => e.preventDefault()}
         />
