4eca58d32afbdd05103ec7cb1cb5eaae9597ef65
Hooobot <hoobot0@gmail.com>
2023-09-27 12:59:14 -1000

    Update IconList to function with Canvas and App

--- frontend/src/IconList.js
+++ frontend/src/IconList.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const IconList = ({ canvasRef }) => {
+const IconList = () => {
   const handleDragStart = (e, iconName) => {
-    e.dataTransfer.setData('iconName', iconName);
-  };
-
-  const handleDrop = (e) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const iconName = e.dataTransfer.getData('iconName');
-
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    // For demonstration, we'll just draw the icon name as text
-    ctx.fillText(iconName, x, y);
+    e.dataTransfer.setData("iconName", iconName);
   };
 
   return (
@@ -24,20 +11,16 @@ const IconList = ({ canvasRef }) => {
       <div
         className="icon"
         draggable
-        onDragStart={(e) => handleDragStart(e, 'Icon1')}
+        onDragStart={(e) => handleDragStart(e, "Icon1")}
       >
         1
       </div>
       <div
         className="icon"
         draggable
-        onDragStart={(e) => handleDragStart(e, 'Icon2')}
+        onDragStart={(e) => handleDragStart(e, "Icon2")}
       >
         2
-      </div>
-      {/* Attach the drop event to the canvas */}
-      <div style={{ display: 'none' }}>
-        <canvas ref={canvasRef} onDrop={handleDrop} onDragOver={(e) => e.preventDefault()} />
       </div>
     </div>
   );
